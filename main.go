70e124e651e8aed32e1ecccfb1da92359a627f9f
nopdan <me@nopdan.com>
2023-04-14 23:11:51 +0800

    feat: 命令行交互

--- main.go
+++ main.go
@@ -14,14 +14,22 @@ import (
 
 func main() {
 
-	if len(os.Args) == 1 {
-		fmt.Println("蔷薇词库转换v1.0\nhttps://github.com/flowerime/rose")
-	}
-
-	if len(os.Args) != 3 {
+	switch len(os.Args) {
+	case 1:
+		ask()
+		return
+	case 2:
+		switch os.Args[1] {
+		case "-v", "version":
+			fmt.Println("蔷薇词库转换v1.0\nhttps://github.com/flowerime/rose")
+			return
+		}
+	case 3:
+	default:
 		wrong()
 		return
 	}
+
 	path := os.Args[1]
 	format := os.Args[2]
 	tmp := strings.Split(format, ":")
@@ -30,6 +38,10 @@ func main() {
 		return
 	}
 	iFormat, oFormat := tmp[0], tmp[1]
+	convert(path, iFormat, oFormat)
+}
+
+func convert(path, iFormat, oFormat string) {
 	if iFormat == "" {
 		iFormat = "dd"
 	}
@@ -43,9 +55,32 @@ func main() {
 	}
 	data := rose.Generate(d, oFormat)
 	od := rose.NewFormat(oFormat).GetDict()
-	os.WriteFile(filepath.Base(path)+"_"+oFormat+"."+od.Suffix, data, 0666)
+	oPath := filepath.Base(path) + "_" + oFormat + "." + od.Suffix
+	err := os.WriteFile(oPath, data, 0666)
+	if err == nil {
+		fmt.Println("转换成功，输出到", oPath)
+	}
 }
 
 func wrong() {
 	fmt.Println("输入参数有误")
+}
+
+func ask() {
+	var path string
+	fmt.Print("待转换的码表：\n> ")
+	fmt.Scanf("%s\n", &path)
+	fmt.Println()
+
+	var iFormat string
+	fmt.Print("待转换码表的格式：\n> ")
+	fmt.Scanf("%s\n", &iFormat)
+	fmt.Println()
+
+	var oFormat string
+	fmt.Print("转换为的格式：\n> ")
+	fmt.Scanf("%s\n", &oFormat)
+	fmt.Println()
+
+	convert(path, iFormat, oFormat)
 }
